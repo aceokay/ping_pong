688f30a1fd6cc89e8498d46f6396d0d39114c98c
Austin Kincaid & Michael Summers <student@epicodus.com>
2015-08-03 13:35:30 -0700

    add method to turn a number that is divisible by both 3 and 5 into 'ping-pong'

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,12 +3,14 @@ class Fixnum
     number_array = []
     count = 1
     self.times() do
-      if (count % 3 == 0)
-          number_array.push('ping')
+      if (count % 3 == 0 && count % 5 == 0)
+        number_array.push("ping-pong")
+      elsif (count % 3 == 0)
+        number_array.push('ping')
       elsif (count % 5 == 0)
-          number_array.push('pong')
+        number_array.push('pong')
       else
-          number_array.push(count)
+        number_array.push(count)
       end
       count += 1
     end
