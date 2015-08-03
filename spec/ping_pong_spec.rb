71f537b31081a449c7981085958aeee7dedb74af
Austin Kincaid & Michael Summers <student@epicodus.com>
2015-08-03 12:20:26 -0700

    Define a spec to test that numbers divisible by 5 display 'pong' instead.

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -10,4 +10,8 @@ describe('Fixnum#ping_pong') do
   it("displays 'ping' if the number is divisible by 3") do
     expect((3).ping_pong()).to(eq([1,2,"ping"]))
   end
+
+  it("displays 'pong' if the number is divisible by 5") do
+    expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
