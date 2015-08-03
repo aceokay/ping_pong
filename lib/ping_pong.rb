4f5659bd5937809d95b1bfb9f8ee3e02653794fe
Austin Kincaid & Michael Summers <student@epicodus.com>
2015-08-03 13:28:18 -0700

    refactor code and implemente code to display pong when divisible by 5

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,12 +3,21 @@ class Fixnum
     number_array = []
     count = 1
     self.times() do
-      case count
-      when 3
-        number_array.push("ping")
+      if (count % 3 == 0)
+          number_array.push('ping')
+      elsif (count % 5 == 0)
+          number_array.push('pong')
       else
-        number_array.push(count)
+          number_array.push(count)
       end
+    #   case count
+    #   when count%3 == (0) #The issue is definitely doing math on it
+    #     number_array.push("ping")
+    #   when count % 5 == 0
+    #     number_array.push("pong")
+    #   else
+    #     number_array.push(count)
+    #   end
       count += 1
     end
     number_array
